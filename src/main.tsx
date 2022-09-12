7b023d85b5cc21673c146ac72f816b74fdaeefa5
with-key <rem9238@kakao.com>
2022-09-13 01:12:45 +0900

    chore: apollo client

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { ApolloProvider, gql } from "@apollo/client";
 import App from "./App";
-import "./index.css";
+import client from "./apollo";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
   </React.StrictMode>
 );
